c6e7db9aac5ffd8e0bcb76342428e22ef0ee1dc3
Sergio <sergiolopezelio@hotmail.com>
2025-03-09 11:49:34 +0100

    Mejora en la renderización

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -3,44 +3,50 @@ import datos from "./data.js";
 import Imagenes from "./Imagenes";
 import "./index.css";
 
-
 const Titulo = () => {
-    const categorias = [...new Set(datos.map(categ => categ.categoria))];
+    const categorias = [...new Set(datos.map((categ) => categ.categoria))];
 
     const [indiceCategoria, setIndiceCategoria] = useState(0);
     const [indiceImagen, setIndiceImagen] = useState(0);
+    const [contador, setContador] = useState(0);
 
     const categoriaActual = categorias[indiceCategoria];
-
     const imagenesPorCategoria = datos.filter((imagen) => imagen.categoria === categoriaActual);
 
     const cambiarImagen = () => {
-        setTimeout(() => {
-            setIndiceImagen((indice) => {
-                if (indice < 2) {
-                    return indice + 1;
-                } else {
+        setIndiceImagen(indiceActualImagen => {
+            if ((indiceActualImagen + 1) > 2) {
+                setIndiceCategoria(indiceActualCategoria => {
+                    if ((indiceActualCategoria + 1) > 2) {
+                        console.log("Reinicio indice categoria.");
                     return 0;
-                }
+                    } 
+                    console.log("Cambio indice categoria.");
+                    return indiceActualCategoria + 1;
+                    
+                });
+                console.log("Reinicio indice imagen.");
+                return 0;
             }
-        )}, 2000);
-    }
+            console.log("Cambio indice imagen.");
+            return indiceActualImagen + 1;
+        });
+        renderizar();
+    };
 
-    const cambiarCategoria = () => {
+    const renderizar = () => {
         setTimeout(() => {
-            setIndiceCategoria((indice) => {
-                if (indice < 2) {
-                    return indice + 1;
-                } else {
-                    return 0;
-                }
-            });
-            setIndiceImagen(0);
-        }, 6000);
+            cambiarImagen();
+        }, 10000);
+    };
+
+    if (contador === 0) {
+        setContador(contadorActual => contadorActual + 1);
+        renderizar();
     }
 
-    cambiarImagen();
-    cambiarCategoria();
+    const lastSlide = (indiceImagen - 1);
+    const nextSlide = (indiceImagen + 1);
 
     return (
         <section className="section">
@@ -50,14 +56,24 @@ const Titulo = () => {
                 <h3>{categoriaActual}</h3>
             </div>
             <div className="section-center">
-                <Imagenes key={imagenesPorCategoria[indiceImagen].id} 
-                            img={imagenesPorCategoria[indiceImagen].img} 
-                            src={imagenesPorCategoria[indiceImagen].src} 
-                            className="activeSlide" />
+                {imagenesPorCategoria.map((imagen, indice) => {
+                    let clase = "";
+                    if (indice === indiceImagen) clase = "activeSlide";
+                    if (indice === lastSlide) clase = "lastSlide";
+                    if (indice === nextSlide) clase = "nextSlide";
+
+                    return (
+                        <Imagenes
+                            key={imagen.id}
+                            img={imagen.img}
+                            src={imagen.src}
+                            className={clase}
+                        />
+                    );
+                })}
             </div>
         </section>
-    )
-}
-
+    );
+};
 
 export default Titulo;
